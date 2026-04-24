9258f94c8c87608d5c3b74938deb1cc275111466
Ted karani <ted.karani@student.moringaschool.com>
2026-04-24 17:56:05 +0300

    assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (event)=>{
+
+    event.preventDefault();
+    const data = document.querySelector("#new-task-description").value;
+    function buildToDo(data){
+      const tasks = document.querySelector("#tasks")
+      const li = document.createElement("li")
+      li.textContent = data;
+
+      tasks.appendChild(li)
+    }
+
+    
+    buildToDo(data)
+  });
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
